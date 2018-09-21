d21474cd34bae49ebe440af698f9c2a14be6a761
Olaf Flebbe <of@oflebbe.de>
2018-09-22 00:06:41 +0200

    clean up tamagochi

--- public/tamagochi.js
+++ public/tamagochi.js
@@ -21,12 +21,3 @@ var make_tamagochi = function () {
     return thus;
 }
 
-function print_state( state) {
-    console.log(state);
-}
-
-/*var tamagochi = make_tamagochi();
-tamagochi.subscribe( print_state);
-tamagochi.update("goo", "bar")
-tamagochi.update("goo", "wfhskdfh")
-tamagochi.update("foo", "bla")*/
