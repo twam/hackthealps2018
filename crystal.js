d8f08b705067f472010a05a07817808651b1edc1
Carsten Lindstedt <carsten.lindstedt@gmail.com>
2018-09-22 14:45:10 +0200

    design

--- crystal.js
+++ crystal.js
@@ -65,7 +65,7 @@ function crystal_init() {
             bevelThickness: (Math.random() * 10) + 25 //length of top
         };
 
-        colors = [ 0xff3333, 0x33ff33, 0x3333ff, 0xffff33 ];
+        colors = [ 0xfa4b69, 0xc8f26d, 0xfacf47, 0xb5246d ];
 
         addShape(
             hexShape,
@@ -84,7 +84,7 @@ function crystal_init() {
     renderer = new THREE.WebGLRenderer({
         antialias: true
     });
-    renderer.setClearColor(0x12d2d3);
+    renderer.setClearColor(0x2d2d36);
     renderer.setPixelRatio(window.devicePixelRatio);
 
     renderer.setSize(crystal_width, crystal_height);
