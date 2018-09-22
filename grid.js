984f8001acd522024e14fc093740c52e0a16e0f0
Carsten Lindstedt <carsten.lindstedt@gmail.com>
2018-09-22 23:22:47 +0200

    yo

--- grid.js
+++ grid.js
@@ -18,15 +18,15 @@ if (!inEditMode) {
 function subscribeGridItem(definition) {
   var formatter = definition.formatter;
   var type = definition.type;
-  
+
   definition.unsubscribe();
   definition.subscribe(function(value) {
       if ($.isNumeric(value)) {
           updateGridItem(definition, formatter(value));
       } else {
           updatePoiItem(definition, formatter(value.numberOfPois), value.lastPoi);
-		  
-		  COBI.app.textToSpeech.write({content : "Have a beer in " + value.lastPoi, language : "en-US"});
+
+		  COBI.app.textToSpeech.write({content : "Grab a 🍺 in " + value.lastPoi, language : "en-US"});
 		  presentSnackbar(value.lastPoi);
 		  //var toast = value.lastPoi;
 		  //M.toast({html: toast, displayLength: 5000, classes: "rounded"});
@@ -70,10 +70,10 @@ function updateGridItem(definition, value) {
 
 function presentSnackbar(poi) {
     var x = document.getElementById("snackbar");
-	x.innerHTML = poi + " visited";
-    
+	x.innerHTML = "Grab a 🍺 in " + poi;
+
 	x.className = "show";
-    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 4000);
 }
 
 $(window).on('load', function(e) {
