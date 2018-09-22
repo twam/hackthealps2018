7bed05cac9ba68ce11f2958e3be4f9dde6afe6e5
Olaf Flebbe <of@oflebbe.de>
2018-09-22 12:33:10 +0200

    simplify

--- crystal.js
+++ crystal.js
@@ -12,11 +12,14 @@ var windowHalfY = window.innerHeight / 2;
 
 function crystal_init() {
     container = document.createElement('div');
-    $('#target').append(container);
+    $('#tamagochi').append(container);
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+    var width = container.offsetWidth;
+    var height = window.innerHeight;
+
+    camera = new THREE.PerspectiveCamera(50, width / height, 1, 1000);
     camera.position.z = 750;
     scene.add(camera);
 
@@ -77,56 +80,11 @@ function crystal_init() {
     });
     renderer.setClearColor(0x2d2d36);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    renderer.setSize(width, height);
     container.appendChild(renderer.domElement);
 
-    document.addEventListener('mousedown', onDocumentMouseDown, false);
-    document.addEventListener('touchstart', onDocumentTouchStart, false);
-    document.addEventListener('touchmove', onDocumentTouchMove, false);
-
     window.addEventListener('resize', onWindowResize, false);
-}
-
-function onDocumentMouseDown(event) {
-    event.preventDefault();
-    document.addEventListener('mousemove', onDocumentMouseMove, false);
-    document.addEventListener('mouseup', onDocumentMouseUp, false);
-    document.addEventListener('mouseout', onDocumentMouseOut, false);
-    mouseXOnMouseDown = event.clientX - windowHalfX;
-    targetRotationOnMouseDown = targetRotation;
-}
-
-function onDocumentMouseMove(event) {
-    mouseX = event.clientX - windowHalfX;
-    targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.02;
-}
-
-function onDocumentMouseUp(event) {
-    document.removeEventListener('mousemove', onDocumentMouseMove, false);
-    document.removeEventListener('mouseup', onDocumentMouseUp, false);
-    document.removeEventListener('mouseout', onDocumentMouseOut, false);
-}
-
-function onDocumentMouseOut(event) {
-    document.removeEventListener('mousemove', onDocumentMouseMove, false);
-    document.removeEventListener('mouseup', onDocumentMouseUp, false);
-    document.removeEventListener('mouseout', onDocumentMouseOut, false);
-}
-
-function onDocumentTouchStart(event) {
-    if (event.touches.length == 1) {
-        event.preventDefault();
-        mouseXOnMouseDown = event.touches[0].pageX - windowHalfX;
-        targetRotationOnMouseDown = targetRotation;
-    }
-}
-
-function onDocumentTouchMove(event) {
-    if (event.touches.length == 1) {
-        event.preventDefault();
-        mouseX = event.touches[0].pageX - windowHalfX;
-        targetRotation = targetRotationOnMouseDown + (mouseX - mouseXOnMouseDown) * 0.05;
-    }
 }
 
 function onWindowResize() {
@@ -140,11 +98,9 @@ function onWindowResize() {
 }
 
 function crystal_animate() {
+    targetRotation = 0.1;
     requestAnimationFrame(crystal_animate);
-    render();
-}
 
-function render() {
-    group.rotation.y += ( targetRotation - group.rotation.y ) * 0.05;
+    group.rotation.y += targetRotation ;
     renderer.render(scene, camera);
 }
